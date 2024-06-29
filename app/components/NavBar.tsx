47ca7469c14e4d9ab0f258d77721074e0a257028
bachidze <bachidze90@gmail.com>
2024-06-29 14:04:51 +0400

    Feat(Add):HeadLine

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -9,6 +9,9 @@ export default function NavBar() {
       <div className="w-[40px] h-[40px] rounded-[50%] border-2 border-[purple] "></div>
       <div className="w-[40px] h-[40px] rounded-[50%] border-2 border-[red] "></div>
     </div>
+    <div>
+      <h1 className="text-white">First Request delay  by 50 seconds or more.</h1>
+    </div>
     <div className="flex gap-4 justify-center pt-8 text-[#C1D7EC]">
       <Link href={"/"}>Home Page</Link>
       <Link href={"/create"}>Create Product</Link>
